2281a2d2d4e16d2b37d3fa06f5a3d4127f969cb4
Odd Andre Owren <oddaow@stud.ntnu.no>
2019-02-14 12:28:43 +0100

    Remove files that were wrongly commited

--- firebase-react/src/components/Firebase/index.js
+++
@@ -1,7 +0,0 @@
-import FirebaseContext, {withFirebase} from './context';
-import Firebase from './firebase';
-
-/*For each component, it is recommended to keep everything of exports in index.js-files for easier import-management.*/
-export default Firebase;
-
-export { FirebaseContext,withFirebase };
