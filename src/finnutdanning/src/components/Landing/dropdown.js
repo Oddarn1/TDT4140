de94d8377b267de95bb0a982e2197f09ee3c3168
Markus Kongstein Rognlien <markukro@stud.ntnu.no>
2019-02-25 16:46:06 +0100

    Fix data syntax and iterating for buttons

--- src/finnutdanning/src/components/Landing/dropdown.js
+++ src/finnutdanning/src/components/Landing/dropdown.js
@@ -7,8 +7,22 @@ class Dropdown extends Component{
 
 
     render(){
+
+        // Henter json-filen som har alle mulige interesser og koblingen til studieretninger
+        var jsonData = require('../../data/interests');
+        
+        // Oppretter tomt array for å lagre interesser
+        var interests = []
+
+        Object.keys(jsonData).forEach(function(interest){
+            interests.push(interest)
+        })
+        
         return(
             <div>
+                {interests.map(function(interest){
+                    return <button value = {interest} onClick = {"placeholder".capture}>{interest}</button>
+                })}
                 <button value="Fotball" onClick={this.props.capture}>Fotball</button>
                 <button value="Sudoku" onClick={this.props.capture}> Sudoku </button>
                 <button value="Helse" onClick={this.props.capture}> Helse </button>
