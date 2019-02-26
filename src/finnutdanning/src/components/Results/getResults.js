b14470f6d7a9607627b6c30264447f5acba8009c
Markus Kongstein Rognlien <markukro@stud.ntnu.no>
2019-02-26 13:44:57 +0100

    Improve search-algorithm

--- src/finnutdanning/src/components/Results/getResults.js
+++ src/finnutdanning/src/components/Results/getResults.js
@@ -18,28 +18,44 @@ export class GetResults extends Component {
     // Lager en liste som i utgangspunktet sier at interessen ikke har noen studieretninger
     var listOfStudyProgramme = <li> Ingen studieretninger </li>;
     // En liste med alle studieretninger som kommer fra interessene
-    var studiesList = []
-
+    var studiesList = [];
+    
     // Itererer gjennom alle interessene
     interests.forEach(interest => {
       // Sjekker om interessen eksisterer i json filen
       if (interest in jsonData) {
         // Henter ut en array med strings som er studieretninger
         var studies = eval("jsonData." + interest);
+        // Regner ut en vekting basert på antall studier mappet til interessen
+        var weight = 1 + 1/studies.length
         // Itererer gjennom alle studieretningene som tilhører interessen
         studies.forEach(study => {
             // Sjekker om studieretningen allerede er i listen
-            if (!studiesList.includes(study)) {
-              // Om det er en ny studieretning settes den bakerst i listen
-              studiesList.push(study);
+            if (!studiesList.some(e => e.name === study)) {
+              // Lager et nytt objekt for denne studieretningen
+              const newStudy = {studyProgramme: study, relevance: 0, reason: []}
+              // Dette objektet legges til listen
+              studiesList.push(newStudy);
             };
+            // Oppdaterer liste over studier
+            studiesList.forEach(element =>{
+              if (element.name == study){
+                element.relevance += weight;
+                element.reason.push(interest);
+              }
+            })
         });
       }
     });
+    
+    // Sorterer listen basert på relevans
+    studiesList.sort(function(a, b){
+      return a.relevance - b.relevance;
+    });
 
-    // Lager en htlm-liste for alle studieretningene som matchet med søket
-    listOfStudyProgramme = studiesList.map((studie) =>
-        <li> {studie} </li>
+    // Lager en html-liste for alle studieretningene som matchet med søket
+    listOfStudyProgramme = studiesList.slice(0,5).map((studie) =>
+        <li> {studie.studyProgramme} </li>
     );
 
     return(
