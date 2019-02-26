7995eb8dce9087bfe0b8ed929e8a0d60ea2aa549
Markus Kongstein Rognlien <markukro@stud.ntnu.no>
2019-02-26 12:13:08 +0100

    Clarify comments

--- src/finnutdanning/src/components/Landing/dropdown.js
+++ src/finnutdanning/src/components/Landing/dropdown.js
@@ -14,11 +14,14 @@ class Dropdown extends Component{
         // Oppretter tomt array for å lagre interesser
         var interests = []
 
+        // Fyller arrayet med alle mulige interesser
         Object.keys(jsonData).forEach(function(interest){
             interests.push(interest)
         })
         
         return(
+            // Itererer gjennom alle interessene for å lage knapper
+            // "Placeholder" blir ikke registrert i søkefelt...
             <div>
                 {interests.map(function(interest){
                     return <button value = {interest} onClick = {"placeholder".capture}>{interest}</button>
