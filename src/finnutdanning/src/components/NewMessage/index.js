6c943ae212f4ab9fd79fd80af0c6a23b0e982c62
Hedda <heddaah@stud.ntnu.no>
2019-03-10 22:00:56 +0100

    add onsubmit newmessage

--- src/finnutdanning/src/components/NewMessage/index.js
+++ src/finnutdanning/src/components/NewMessage/index.js
@@ -1,48 +1,73 @@
 import React, {Component} from 'react';
 import withAuthorization from "../Session/withAuthorization";
+import {AuthUserContext} from '../Session';
+import * as ROLES from '../../constants/roles';
+import withAuthentication from "../Session/withAuthentication";
+import {withFirebase} from '../Firebase';
+import firebase from 'firebase';
 
 
-
+const INITIAL_STATE = {
+       to: "",
+       content: ""
+}
 
 class NewMessage extends Component{
      constructor(props){
         super(props);
-        this.state={
-            to: "",
-            subject: "",
-            content: ""
-        };
+        this.state={...INITIAL_STATE};
         this.onChange=this.onChange.bind(this);
-}
+        this.onSubmit=this.onSubmit.bind(this);
+     }
 
        onChange(event){
        this.setState({[event.target.name]:event.target.value});
        }
-       
 
+       onSubmit = event => {
+       const {content, to} = this.state;
+       const senderid = this.props.firebase.auth.currentUser.uid;
+       const recpid = "";
+       const first = true;
+       this.props.firebase
+       .messages().push({senderid, recpid, content})
+       .then(() => {
+              event.preventDefault();
+              this.setState({...INITIAL_STATE});
+
+       });
+
+};
+
+// hente brukerid currentuser : this.props.firebase.auth.currentUser.uid
+
+// "sende" melding this.props.firebase.messages().push({senderid: ^,content})
+// .then(this.setState({standard}))
+// .catch(error)
 
 
 
     render() {
 
+       const {to, content} = this.state;
+       const isInvalid = content === '';
+
 
       return (
 
+
         <div>
+        <form onSubmit={this.onSubmit}>
         <label>Til </label>
+
+       {this.props.firebase.auth.currentUser === ROLES.ADMIN &&
                 <input value={this.state.to}
                        placeholder="Mottakere"
                        onChange={this.onChange}
                        name="to"
                        />
-                       <br/>
+                }
 
-
-                <input value={this.state.subject}
-                       placeholder="Emne"
-                       onChange={this.onChange}
-                       name="subject"
-                       />
                        <br/>
 
 
@@ -53,16 +78,21 @@ class NewMessage extends Component{
                        />
                        <br/>
 
-                       <button type="submit">Send </button>
 
 
 
+                       <button disabled={isInvalid} type="submit">Send </button>
+
+
+
+        </form>
         </div>
-      )
+
+)
 
 
 
-    }
+}
 
 }
 
